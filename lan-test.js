7b464ab1820c01355a8772ba8b98b85fc67f3a22
PhongDang <phongdang707@gmail.com>
2023-11-25 20:26:47 +0700

    chore: update

--- lan-test.js
+++ lan-test.js
@@ -1,1 +1,11 @@
-const tbc = (a, b) => (a + b) / 2;
+const tbc = (a, b) => {
+  if (typeof a !== "number") {
+    throw new Error("a must be a number");
+  }
+
+  if (typeof b !== "number") {
+    throw new Error("b must be a number");
+  }
+
+  return (a + b) / 2;
+};
