baa4958ad6d55c3a3ab8645d38e03c5710092e5e
PhongDang <phongdang707@gmail.com>
2023-11-25 20:49:42 +0700

    chore: update

--- khoa.js
+++ khoa.js
@@ -5,3 +5,17 @@ const hello = (a, b) => "hello world";
 const abc = (a, b) => {
   return "ahihi";
 };
+
+const tbc = (a, b) => {
+  if (typeof a !== "number") {
+    throw new Error("a must be a number");
+  }
+
+  if (typeof b !== "number") {
+    throw new Error("b must be a number");
+  }
+
+  return (a + b) / 2;
+};
+
+const hello2 = (a, b) => {};
