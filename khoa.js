d6f069c87d0bb2ea5a3515d7b8271a6615962be6
PhongDang <phongdang707@gmail.com>
2023-11-25 20:40:45 +0700

    chroe: update

--- khoa.js
+++ khoa.js
@@ -1,3 +1,15 @@
 const sum = (a, b) => a + b;
 
 const hello = (a, b) => "hello world";
+
+const tbc = (a, b) => {
+  if (typeof a !== "number") {
+    throw new Error("a must be a number");
+  }
+
+  if (typeof b !== "number") {
+    throw new Error("b must be a number");
+  }
+
+  return (a + b) / 2;
+};
